9039d1c693676be9293cfbea8a03d90591525f75
Mathieu Dugre <mathieu.dugre@mdugre.info>
2025-03-01 09:36:42 -0500

    Changing build detection of dev

--- vite.config.ts
+++ vite.config.ts
@@ -3,25 +3,27 @@ import react from '@vitejs/plugin-react'
 // import { comlink } from "vite-plugin-comlink";
 import fs from 'fs';
 
-// https://vite.dev/config/
-const config = {
-    base: '/dataviewer',
-    plugins: [
-        // comlink(),
-        react(),
-    ]
-} as UserConfig;
+export default defineConfig(({command})=> {
+    // https://vite.dev/config/
+    const config = {
+        base: '/dataviewer',
+        plugins: [
+            // comlink(),
+            react(),
+        ]
+    } as UserConfig;
 
-// if env DEV
-if (process.env.NODE_ENV === 'development') {
-    config.server = {
-        https: {
-            key: fs.readFileSync('/var/opt/millegrilles/secrets/pki.nginx.key'),
-            cert: fs.readFileSync('/var/opt/millegrilles/secrets/pki.nginx.cert'),
-        },
-        host: '0.0.0.0',
-        allowedHosts: true
+    // if env is development
+    if(command === 'serve') {
+        config.server = {
+            https: {
+                key: fs.readFileSync('/var/opt/millegrilles/secrets/pki.nginx.key'),
+                cert: fs.readFileSync('/var/opt/millegrilles/secrets/pki.nginx.cert'),
+            },
+            host: '0.0.0.0',
+            allowedHosts: true
+        }
     }
-}
 
-export default defineConfig(config);
+    return config;
+});
